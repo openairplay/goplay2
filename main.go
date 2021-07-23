c093bee307df20fbd1e7d828be3ab5356c87174e
noelhibbard <noelhibbard@users.noreply.github.com>
2021-07-23 17:22:38 -0400

    Adding ALSA volume control (#11)
    * Adding ALSA volume control
    
    * Update README
    
    Co-authored-by: Noel Hibbard <NoelH@jaru.com>

--- main.go
+++ main.go
@@ -20,15 +20,15 @@ import (
 
 func main() {
 	var ifName string
-	var deviceName string
 	var syncLogFile string
 	var delay int64
 
 	flag.StringVar(&ifName, "i", "eth0", "Specify interface")
 	flag.Int64Var(&delay, "delay", 50, "Specify hardware delay in ms (useful on slow computer)")
-	flag.StringVar(&deviceName, "n", "goplay", "Specify device name")
+	flag.StringVar(&config.Config.DeviceName, "n", "goplay", "Specify device name")
 	flag.StringVar(&syncLogFile, "sync", "/dev/null", "Specify audio sync log")
 	flag.StringVar(&config.Config.AlsaPortName, "alsa", "pcm.default", "Specify Alsa Device - Linux only")
+	flag.StringVar(&config.Config.AlsaMixerName, "alsamixer", "disabled", "Specify Alsa Mixer Control - Linux only")
 	flag.Parse() // after declaring flags we need to call it
 
 	globals.ErrLog = log.New(os.Stderr, "Error:", log.LstdFlags|log.Lshortfile|log.Lmsgprefix)
@@ -51,11 +51,11 @@ func main() {
 			ipStringAddr = append(ipStringAddr, v.IP.String())
 		}
 	}
-	homekit.Device = homekit.NewAccessory(macAddress, deviceName, airplayDevice())
+	homekit.Device = homekit.NewAccessory(macAddress, config.Config.DeviceName, airplayDevice())
 	log.Printf("Starting goplay for device %v", homekit.Device)
-	homekit.Server, err = homekit.NewServer(macAddress, deviceName, ipStringAddr)
+	homekit.Server, err = homekit.NewServer(macAddress, config.Config.DeviceName, ipStringAddr)
 
-	server, err := zeroconf.Register(deviceName, "_airplay._tcp", "local.",
+	server, err := zeroconf.Register(config.Config.DeviceName, "_airplay._tcp", "local.",
 		7000, homekit.Device.ToRecords(), []net.Interface{*iFace})
 	if err != nil {
 		panic(err)
@@ -93,7 +93,7 @@ func main() {
 	}()
 
 	go func() {
-		handler, e := handlers.NewRstpHandler(deviceName, player)
+		handler, e := handlers.NewRstpHandler(config.Config.DeviceName, player)
 		if e != nil {
 			panic(e)
 		}
